309a7c3a6c26a98519a9a3e67ffbd1e0b8d2bfa4
vazago <vazago@users.noreply.github.com>
2025-09-22 15:55:02 +0200

    Add files via upload

--- gdu_base/models/gdu_base_area.py
+++ gdu_base/models/gdu_base_area.py
@@ -5,15 +5,15 @@ from odoo import models, fields, api
 class gdu_base_area(models.Model):
     _name = 'gdu.base.area'
 
-    name= fields.Char('Área base', readonly=True)
-    sigla=fields.Char('Sigla',  readonly=True)
+    name = fields.Char('Área base', readonly=True)
+    sigla = fields.Char('Sigla',  readonly=True)
     centro = fields.Char(string='Centro')
     departamentos_ids = fields.One2many('gdu.department', 'area_base_id', 'Departamentos')
     partners_ids = fields.One2many('res.partner','area_base_id', string='Personas')
+    # maykel
+    #rango_inicial = fields.Char('Rango inicial')
+    #rango_final = fields.Char('Rango final')
 
-    rango_inicial = fields.Char('Rango inicial')
-    rango_final = fields.Char('Rango final')
-
-    user_id = fields.Many2one('res.users', string='Jefe de Área')
+    #user_id = fields.Many2one('res.users', string='Jefe de Área')
 
     # user_ids = fields.One2many('comodel_name', 'inverse_field_name', string='user')
